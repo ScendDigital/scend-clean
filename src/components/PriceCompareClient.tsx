f3e99a6069c2e211989cf23ad6b8913b3833269d
Motlatsi Lenyatsa <motlatsi.lenyatsa@gmail.com>
2025-11-18 10:07:00 +0200

    Remove duplicate component folders

--- src/components/PriceCompareClient.tsx
+++
@@ -1,4 +0,0 @@
-﻿"use client";
-import dynamic from "next/dynamic";
-const PriceCompare = dynamic(() => import("../tools/PriceCompare"), { ssr: false });
-export default function PriceCompareClient() { return <PriceCompare />; }
--- src/components/TaxToolClient.tsx
+++
@@ -1,9 +0,0 @@
-﻿"use client";
-import dynamic from "next/dynamic";
-
-// If your TaxTool lives at src/tools/TaxTool.(tsx|ts|jsx|js)
-const TaxTool = dynamic(() => import("../tools/TaxTool"), { ssr: false });
-
-export default function TaxToolClient() {
-  return <TaxTool />;
-}
