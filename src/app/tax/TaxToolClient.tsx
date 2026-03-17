f050ebbc4efc78b316f0e957e4886d1dca5b2fdf
Motlatsi Lenyatsa <motlatsi.lenyatsa@gmail.com>
2026-03-17 03:37:15 +0200

    Fix TaxTool import path to correct location

--- src/app/tax/TaxToolClient.tsx
+++ src/app/tax/TaxToolClient.tsx
@@ -1,9 +1,9 @@
 "use client";
 import dynamic from "next/dynamic";
 
-const TaxTool = dynamic(() => import("../../components/tax/TaxTool"), {
+const TaxTool = dynamic(() => import("../../tools/TaxTool"), {
   ssr: false,
-  loading: () => <div>Loading Tax Tool…</div>,
+  loading: () => <div>Loading Tax Tool...</div>,
 });
 
 export default function TaxToolClient() {
